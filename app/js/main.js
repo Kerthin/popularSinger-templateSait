b8b0ce0eb81de588a1c655a8b4579e318b436a98
Kerthin <belichenko.roden@list.ru>
2018-08-25 14:06:22 +0300

    Adding and connecting a js library such as 'jquery.min.js', 'fullpage.js', 'parallax.min.js', 'TweenLite.min.js', 'EasePack.min.js'

---
+++ app/js/main.js
@@ -0,0 +1,24 @@
+/********************
+	Jquery.min.js
+********************/
+//= partials/libs/jquery.min.js
+
+/********************
+	Fullpage.min.js
+********************/
+//= partials/libs/fullpage.min.js
+
+/********************
+	Parallax.min.js
+********************/
+//= partials/libs/parallax.min.js
+
+/********************
+	EasePack.min.js
+********************/
+//= partials/libs/EasePack.min.js
+
+/********************
+	TweenLite.min.js
+********************/
+//= partials/libs/TweenLite.min.js
